bb2b480ba2db0856e69ac3445c0157debf6a9c81
Paul Spenke <info@onlineer.de>
2023-06-09 10:16:16 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+// Uncomment to disable error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ERROR);
+
+// Uncomment when using composer
+//require_once __DIR__ . '/vendor/autoload.php';
+
+// Uncomment if you downloaded PHPagebuilder
+require_once __DIR__ . '/phpagebuilder/src/Core/helpers.php';
+spl_autoload_register('phpb_autoload');
+
+$config = require __DIR__ . '/config.php';
+
+$builder = new PHPageBuilder\PHPageBuilder($config);
+$builder->handleRequest();
