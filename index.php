563ce925cc1a238546d4cac1ed65f88be4078bd0
Paul Spenke <info@onlineer.de>
2023-06-11 21:15:31 +0800

    update error display

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 // Uncomment to disable error reporting
 ini_set('display_errors', 1);
-error_reporting(E_ERROR);
+error_reporting(E_ALL);
 
 // Uncomment if you downloaded Plugi
 require_once __DIR__ . '/plugi/src/Core/helpers.php';
