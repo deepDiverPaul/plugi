d4c734cdbd07b8e5fc993884247b4b7f12d6fac3
Paul Spenke <info@onlineer.de>
2023-06-13 07:26:52 +0800

    update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 // Uncomment if you downloaded Plugi
-require_once __DIR__ . '/plugi/src/Core/helpers.php';
+require_once __DIR__ . '/plugi-core/src/Core/helpers.php';
 spl_autoload_register('phpb_autoload');
 
 $config = require __DIR__ . '/config.php';
