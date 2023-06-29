77c642984cd6ecb30810f60955bc7e98b7658a32
Paul Spenke <info@onlineer.de>
2023-06-30 07:28:16 +0800

    start implementing DB check view

--- plugi-core/src/Modules/WebsiteManager/resources/views/tabs/info.php
+++ plugi-core/src/Modules/WebsiteManager/resources/views/tabs/info.php
@@ -1,12 +1,29 @@
 <div class="overflow-x-auto">
     <table class="table table-zebra">
         <tbody>
-        <!-- row 1 -->
         <tr>
             <th class="w-60">PHP Version</th>
-            <td><?= phpversion() ?>n</td>
+            <td><?= phpversion() ?></td>
         </tr>
-        <!-- row 2 -->
+        <tr>
+            <th class="w-60">Database</th>
+            <td>
+                <?php
+                global $phpb_db;
+                $tables = include __DIR__.'/../../../../../../config/tables.php';
+                foreach ($tables as $table => $structure) :
+                ?>
+                <li>
+                    <?= phpb_e($table) ?> (<?= phpb_e($phpb_db->tableExists($table) ? 'yes' : 'no') ?>)
+                    <ul>
+                        <li></li>
+                    </ul>
+                </li>
+                <?php
+                endforeach;
+                ?>
+            </td>
+        </tr>
         <tr>
             <th class="align-top">Installed extensions</th>
             <td>
