7b47fdaf14f6d339f1ba1795d77e3e2205cb2f68
Paul Spenke <info@onlineer.de>
2023-07-02 21:46:47 +0800

    update dependencies

--- themes/theme-one/src/main.ts
+++ themes/theme-one/src/main.ts
@@ -1,9 +1,2 @@
 import './style.css'
 // import typescriptLogo from './typescript.svg'
-import 'flowbite';
-
-// @ts-ignore
-const toggleNavbar = (collapseID: string) => {
-    document.getElementById(collapseID)?.classList.toggle("hidden");
-    document.getElementById(collapseID)?.classList.toggle("block");
-}
